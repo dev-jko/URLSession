a970a1c472ce100dd153986a9e3dffaa94ef7b0d
nadarm <ccc7497@gmail.com>
2020-08-21 23:14:05 +0900

    network

--- URLSession/ViewController.swift
+++ URLSession/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    
+    private let network = Network()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url: String = "https://jsonplaceholder.typicode.com/todos/1"
+        
+        network.network(url: url) { [weak self] data, response, error in
+            guard let `self` = self else { return }
+            
+            if let error = error {
+                self.label.text = error.localizedDescription
+                return
+            }
+            
+            guard let data = data else { return }
+            let string = String(data: data, encoding: .utf8)
+            self.label.text = string
+        }
     }
 
+    
 
 }
 
+class Network {
+    
+    func network(url: String, completion: ((Data?, URLResponse?, Error?) -> Void)? = nil) {
+        guard let url = URL(string: url) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                completion?(data, response, error)
+            }
+        }.resume()
+    }
+}
